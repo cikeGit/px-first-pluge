afa0fd5085a27977ca9e0c433ec14ea479ec71e6
GitHub Action <action@github.com>
2023-04-25 02:14:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/cikegit/pxfirstpluge/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cikegit/pxfirstpluge/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.cikegit.pxfirstpluge.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cikegit.pxfirstpluge.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
